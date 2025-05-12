329d303a9aaeb5c7af4289d834961bad564d7263
shruti kathiriya <189000166+ShrutiKathiriya-coder@users.noreply.github.com>
2025-05-12 11:29:41 +0530

    ch-2 program

---
+++ program1.cpp
@@ -0,0 +1,22 @@
+// Addition of 12 and 6 is 18
+
+#include <iostream>
+using namespace std;
+
+
+main()
+{
+    int a =12,b=6;
+    cout << " adddition of " << a << " and " << b << " is " << a+b <<endl;
+    cout << "Subtraction of " << b << " and "  << b << " is " << a-b <<endl;
+    cout << "Multiplication of " << b << " and "  << b << " is " << a*b <<endl;
+    cout << "Division of " << b << " and "  << b << " is " << a/b <<endl;
+    cout << "Modul of " << b << " and "  << b << " is " << a%b <<endl;
+
+
+    cout << a << " + " << b << " = " << a+b << endl;
+    cout << a << " - " << b << " = " << a-b << endl;
+    cout << a << " * " << b << " = " << a*b << endl;
+    cout << a << " / " << b << " = " << a/b << endl;
+    cout << a << " % " << b << " = " << a%b << endl;
+}
